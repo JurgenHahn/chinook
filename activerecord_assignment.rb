1897326bbc1b82517f5acf0d3985dd7c044f1d06
JurgenHahn <jurgenehahn@gmail.com>
2017-02-01 17:31:12 -0500

    assignment finished

--- activerecord_assignment.rb
+++ activerecord_assignment.rb
@@ -5,26 +5,38 @@
 
 
 # 1a) Find the genre with the name "Hip Hop/Rap".
-
+  Genre.find_by(name: 'Hip Hop/Rap')
+  #returns genre_id of 17 among other things
 
 
 # 1b) Count how many tracks belong to the "Hip Hop/Rap" genre
-
-
+  Track.where(genre_id: '17').count
+#returns 35
 
 # 2) Find the total amount of time required to listen to all the tracks in the database.
+  Track.sum("milliseconds")
 
+#returns 1378575827
 
 
 # 3a) Find the highest price of any track that has the media type "MPEG audio file".
+  MediaType.find_by(name: 'MPEG audio file')
+  #returns MediaType id: 1
 
+  Track.where(media_type_id: "1").maximum("unit_price")
+#returns '0.99E0'
 
 
 # 3b) Find the name of the most expensive track that has the media type "MPEG audio file".
+  Track.where(media_type_id: "1")
+Track.where(unit_price: "0.99E0")
+Track.where(media_type_id: "1").where(unit_price: "0.99E0") #.limit(1).first
+
 
 
 
 # 4) Find the 2 oldest artists.
+Artist.order(created_at: :asc).limit(2)
 
 
 
--- sql_assignment.sql
+++ sql_assignment.sql
@@ -5,19 +5,27 @@
 -- below each of the following questions:
 
 -- 1) Find the albums recorded by the artist Queen.
+SELECT id, name FROM artists WHERE name = 'Queen';
+SELECT * FROM albums WHERE artist_id = '51'
 
 
 
 -- 2) [Count](http://www.w3schools.com/sql/sql_func_count.asp) how many tracks belong to the media type "Protected MPEG-4 video file".
-
-
+SELECT * FROM media_types --Protected MPEG-4 video file id == 3
+SELECT COUNT (*) FROM tracks WHERE media_type_id = '3';
 
 -- 3) Find the least expensive track that has the genre "Electronica/Dance".
-
+SELECT * FROM genres --returns Electronica/Dance id == 15
+SELECT name, unit_price FROM tracks WHERE genre_id = '15' ORDER BY unit_price;
 
 
 -- 4) Find the all the artists whose names start with A.
+SELECT name FROM artits WHERE name LIKE 'A%';
 
 
 
 -- 5) Find all the tracks that belong to playlist 1.
+SELECT id, name FROM playlists --Playlist 1 is called Music
+SELECT id, name FROM tracks WHERE id IN (SELECT id FROM playlists_tracks WHERE playlist_id = '1');
+
+--SELECT p.name, p.id AS playlist_id, t.id AS track_id, t.name AS track_name FROM playlists p INNER JOIN playlists_tracks pt ON p.id = pt.playlist_id INNER JOIN tracks t ON pt.track_id = t.id;
